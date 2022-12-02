614ef2b103cb1718399336bc852b9eaf1440679e
Matic Babnik <matic.babnik@outlook.com>
2022-12-02 07:36:33 +0100

    day 2 cleanup

--- d2/index.js
+++ d2/index.js
@@ -1,37 +1,21 @@
 const { readFileSync } = require('fs')
 
+const dec = { A: 1, B: 2, C: 3, X: 1, Y: 2, Z: 3 },
+    win = { 1: 2, 2: 3, 3: 1 },
+    lose = { 1: 3, 2: 1, 3: 2, },
+    wld = {
+        1: x => lose[x],
+        2: x => x, //draw
+        3: x => win[x],
+    };
 
-const wm = { // win map
-    1: 2,
-    2: 3,
-    3: 1
-}
-const lm = { //loose map
-    1: 3,
-    2: 1,
-    3: 2,
-}
-const wld = { // win,loose,draw functions
-    3(x) { return wm[x] },
-    1(x) { return lm[x] },
-    2(x) { return x },
-}
-
-const com = readFileSync('input2').toString()
-    .replace(/(A|X)/g, 1)
-    .replace(/(B|Y)/g, 2)
-    .replace(/(C|Z)/g, 3)
+const com = readFileSync('input2')
+    .toString()
     .split('\n')
-    .map(x => x.split(' ').map(y => y - 0))
+    .map(x => x.split(' ').map(y => dec[y]))
 
+const score = x => x.map(([h, m]) => h == m ? m + 3 : (win[h] == m ? m + 6 : m)).reduce((p, c) => c + p);
 
-const cal = com
-    .map(([h, m]) => h == m ? m + 3 : (wm[h] == m ? m + 6 : m))
-    .reduce((p, c) => c + p);
-
-const cal2 = com
-    .map(([a, b]) => [a, wld[b](a)])
-    .map(([h, m]) => h == m ? m + 3 : (wm[h] == m ? m + 6 : m))
-    .reduce((p, c) => c + p);
+const cal = score(com), cal2 = score(com.map(([a, b]) => [a, wld[b](a)]))
 
 console.log({ cal, cal2 })
