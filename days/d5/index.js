1ee72f16fff785c51ae936e70e37a5a183e70f9b
Matic Babnik <matic.babnik@outlook.com>
2022-12-05 18:45:00 +0100

    saved a few bytes d5

--- days/d5/index.js
+++ days/d5/index.js
@@ -4,7 +4,7 @@ let [s, i] = input('input5', '\n\n', '\n'),
     p = t => console.log(t.map(x => x.at(-1)).join``),
     st = s.map(x => `${x} `.split``.by(4).map(y => y[1]).collect())
         .slice(0, -1).reverse().reduce((a, c) => c.forEach((z, i) => z != ' ' && a[i].push(z)) || a,
-            [...Array(Math.round(s[0].length / 4))].map(x => [])),
+            [...Array((s[0].length + 1) / 4)].map(x => [])),
     z = JSON.parse(JSON.stringify(st)), //backup
     j = i.map(x => x.match(/\d+/g).map(y => y - 1));
 
